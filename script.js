e28cb0c435c82b07d5f584796626d4c3793e414a
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-01-26 12:58:31 +0600

    script js added

---
+++ script.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:7.2
+        }
+    }
+})
